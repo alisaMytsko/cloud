0c5360a40ab635efb42ddd18382210089360ac4d
Aleksander Andrukovich <aleksander.an@playgendary.com>
2021-02-24 23:09:29 +0300

    edit

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { store } from './reducers'
 import { Provider } from 'react-redux';
-import Alert from '../src/utils/alert/alert'
+import Alert from '../src/utils/alert/alert';
+
 
 
 ReactDOM.render(
